2ea811c35ccb8f9175f3e6851552e7aab5875d4c
5CD5064Z2H\Shohei <takeda.syouhei@gmail.com>
2019-05-19 00:21:18 +0900

    Chapter4の全編完成。次はChapter5.関数はできませんでした．

---
+++ Assets/Chapter4/Chapter4-scripts/HiyokoGeenerater.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiyokoGeenerater : MonoBehaviour {
+    public float interval = 3.0f;
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("SpawnObj", 0.1f, interval);
+	}
+    void SpawnObj()
+    {
+        Instantiate(obj, transform.position, transform.rotation);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
