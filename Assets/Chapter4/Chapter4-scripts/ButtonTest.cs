a7e0d295e980d7065ee66cf0fc890fc529d4d034
5CD5064Z2H\Shohei <takeda.syouhei@gmail.com>
2019-05-21 21:47:30 +0900

    Chapter5までできました。

---
+++ Assets/Chapter4/Chapter4-scripts/ButtonTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public void TestStart()
+    {
+        Debug.Log("Hello,World");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
